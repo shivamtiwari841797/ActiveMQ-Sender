fcb2ac9cf4b3dd5b47b7700529839167b5a7008d
Shivam Tiwari <shivamtiwari8417@gmail.com>
2021-09-24 13:02:56 +0530

    Initial commit

---
+++ src/test/java/com/company/activemq/ActiveMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.activemq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
